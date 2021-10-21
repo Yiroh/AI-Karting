eb1827ed3883ad6cd000e30638b58795010676cd
Yiroh <yirohyiroh@gmail.com>
2021-10-21 00:19:04 -0400

    Created Camera and Movement Scripts
    Created Camera and Movement scripts, added some temporary objects in the environment to be able to visually see movement. Camera and Movement definitely need to see more changes still.

---
+++ Karting AI/Assets/CameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+    public Vector3 eulerRotation;
+    public float damper;
+
+    void Start()
+    {
+        transform.eulerAngles = eulerRotation;
+    }
+
+    void Update()
+    {
+        if (target == null)
+            return;
+
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, damper * Time.deltaTime);
+    }
+}
